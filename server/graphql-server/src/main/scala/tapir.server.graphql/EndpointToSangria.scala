57a3a63e7f1e8d10306737124b985dcfb209ddd2
kell18 <albert.bikeev@gmail.com>
2020-01-20 23:57:29 +0100

    Some work on Schema mapping

--- server/graphql-server/src/main/scala/tapir.server.graphql/EndpointToSangria.scala
+++ server/graphql-server/src/main/scala/tapir.server.graphql/EndpointToSangria.scala
@@ -1,8 +1,11 @@
 package tapir.server.graphql
 
+import sangria.ast
+import sangria.marshalling.MarshallerCapability
 import sangria.schema.{Schema => SangriaSchema, _}
+import sangria.validation.{BigIntCoercionViolation, IntCoercionViolation}
 import sttp.tapir.{Codec, CodecForMany, CodecFormat, CodecMeta, Endpoint, EndpointInfo, EndpointInput, Schema}
-import sttp.tapir.SchemaType.SString
+import sttp.tapir.SchemaType.{SInteger, SObjectInfo, SProduct, SString}
 import scala.reflect.ClassTag
 
 object EndpointToSangria {
@@ -21,6 +24,8 @@ object EndpointToSangria {
         implicit outType: OutputType[O],
         ev: ValidOutType[Res, O]
     ): Field[Ctx, Any] = {
+
+      // .. bigger than schemaToSInputType - because not every ep input contains Schema
       def inputToArgs(input: EndpointInput[I]): List[Argument[_]] = {
         input match {
           case EndpointInput.Query(qName, qCodec, qInfo) =>
@@ -33,18 +38,62 @@ object EndpointToSangria {
       Field(name, outType, description, args, resolve)
     }
 
-    def argTypeFromCodec[T: ClassTag, CF <: CodecFormat, R](meta: CodecMeta[T, CF, R]): InputType[T] = {
+    def schemaToSInputType[T: ClassTag](schema: Schema[T]): InputType[T] = {
       val tName = implicitly[ClassTag[T]].getClass.getSimpleName
-      meta.schema match {
+      (schema match {
         case Schema(SString, _, description, _) =>
-          ScalarType(tName, description, coerceUserInput = { any =>
-            Right(any.toString)
-          }, coerceOutput = { case (t, _) => t.toString }, coerceInput = { v =>
-            Right(v.renderPretty)
-          })
+          ScalarType[String](
+            tName,
+            description,
+            coerceUserInput = { any =>
+              Right(any.toString)
+            },
+            coerceInput = { v =>
+              Right(v.renderPretty)
+            },
+            // .. make sure it's correct
+            coerceOutput = { case (t, _) => t.toString }
+          ).asInstanceOf[ScalarType[T]]
+        case Schema(SInteger, _, description, _) =>
+          ScalarType[Int](
+            tName,
+            description,
+            coerceUserInput = {
+              case i: Int ⇒ Right(i)
+              case i: Long if i.isValidInt ⇒ Right(i.toInt)
+              case i: BigInt if !i.isValidInt ⇒ Left(BigIntCoercionViolation)
+              case i: BigInt ⇒ Right(i.intValue)
+              case d: Double if d.isValidInt ⇒ Right(d.intValue)
+              case d: BigDecimal if d.isValidInt ⇒ Right(d.intValue)
+              case _ ⇒ Left(IntCoercionViolation)
+            },
+            coerceInput = {
+              case ast.IntValue(i, _, _) ⇒ Right(i)
+              case ast.BigIntValue(i, _, _) if !i.isValidInt ⇒ Left(BigIntCoercionViolation)
+              case ast.BigIntValue(i, _, _) ⇒ Right(i.intValue)
+              case _ ⇒ Left(IntCoercionViolation)
+            },
+            coerceOutput = valueOutput
+          )
+        case Schema(SProduct(SObjectInfo(fullName, tParamsNames), fieldsS), isOptional, description, format) =>
+          val fieldsInpTypes = fieldsS.map { case (name, fSchema) => name -> schemaToSInputType(fSchema) }
+           // TODO provide at least descriptions and maybe defaultValues
+          val inputFields = fieldsInpTypes.map { case (name, iType) => InputField(name, iType, "") }.toList
+          InputObjectType(
+            tName,
+            description,
+            () => inputFields,
+            Vector.empty,
+            Vector.empty
+          )
         // ..
-      }
+        case unknown => sys.error(s"$unknown input schema match is not implemented")
+      }).asInstanceOf[ScalarType[T]]
     }
+
+    // def schemaToSInputField(schema: Schema[_]):
+
+    def valueOutput[T](value: T, capabilities: Set[MarshallerCapability]): T = value
 
     // .. In case we need obj type for each arg
     // def inputToArgsAndTypes(input: EndpointInput[I]): List[(ObjectType[Ctx, Val], Argument[_])] = ???
